62f274f790dc340944933f03b20231d827fc9b48
ShrishShukla <57490117+ShrishShukla@users.noreply.github.com>
2019-11-08 21:25:29 +0530

    Add files via upload

---
+++ TEMPIOTTHING.ino
@@ -0,0 +1,46 @@
+#include "ThingSpeak.h"
+#include <ESP8266WiFi.h>
+const int LM35 = A0;
+//----------- Enter you Wi-Fi Details---------//
+char ssid[] = "AndroidAP48E0"; //SSID
+char pass[] = "tejn1097"; // Password
+//-------------------------------------------//
+
+WiFiClient  client;
+
+unsigned long myChannelNumber = 000000; // Channel ID here
+const int FieldNumber = 1;
+const char * myWriteAPIKey = "2U4Z369OTN049AVY"; // Your Write API Key here
+
+void setup()
+{
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  ThingSpeak.begin(client);
+}
+void loop()
+{
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+      WiFi.begin(ssid, pass);
+      Serial.print(".");
+      delay(5000);
+    }
+    Serial.println("\nConnected.");
+  }
+  int ADC;
+  float temp;
+  ADC = analogRead(LM35);  /* Read Temperature */
+  temp = (ADC * 3); /* Convert adc value to equivalent voltage */
+  temp = (temp / 10); /* LM35 gives output of 10mv/°C */
+  Serial.print("Temperature = ");
+  Serial.print(temp);
+  Serial.println(" *C");
+  delay(1000);
+  ThingSpeak.writeField(myChannelNumber, FieldNumber, temp, myWriteAPIKey);
+  delay(1000);
+}
